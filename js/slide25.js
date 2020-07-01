5df7b20dcf7e58b6c48cbfe74ed75cf8e4cc5b94
dmholland <kenoshatrader@gmail.com>
2020-07-01 14:00:17 -0400

    Done

--- js/slide25.js
+++ js/slide25.js
@@ -2,8 +2,8 @@ function input(){
     var userIn;
     do{
     userIn = [prompt("Please enter the year"),prompt("Please enter the month")];}
-    while(isNAN(userIn[0]) || isNAN(userIn[0]) || userIn[0].length != 4 || userIn[1] <=12 || userIn[1] == 0);
-
-
+    while(isNaN(userIn[0]) || isNaN(userIn[0]) || userIn[0].length != 4 || userIn[1] >12 || userIn[1] == 0);
+let numOfDays = 32 - new Date(userIn[0],userIn[1],32).getDate();
+document.write("The number of days are "+numOfDays);
 }
 input();
