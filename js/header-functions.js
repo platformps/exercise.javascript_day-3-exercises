d5f03a6a0773b85d07652dfeb2cb5fde4619c237
monicadeshmukh <renavikar_monica@yahoo.com>
2020-07-01 19:24:52 -0400

    updated header and footer.js

--- js/header-functions.js
+++ js/header-functions.js
@@ -1,1 +1,30 @@
 // OPTIONAL TODO; define implementation-specific tests here
+function testIsNumber(){
+    //console.log("inside testIsNumber");
+    var expression = window.prompt("input: ");
+    console.log("output: " + isNumber(expression));
+    
+  }
+
+  function testconvertToNumber(){
+    //console.log("inside testIsNumber");
+    var expression = window.prompt("input: ");
+    console.log("output: " + convertToNumber(expression));
+    
+  }
+
+  function testsmaller() {
+    console.log(smaller());
+  }
+
+  function testsmaller3() {
+    console.log(smaller3());
+  }
+
+  function testleap() {
+    console.log(leap());
+  }
+
+  function testmonth() {
+    console.log(month());
+  }
