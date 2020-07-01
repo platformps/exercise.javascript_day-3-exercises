2fb2ca73e7a8adc897ba8216f76a5eff5439f5d1
suresh kandagatla <surikandagatla@gmail.com>
2020-07-01 15:36:27 -0400

    Made changes: created usefulFunctions.js moved all functions there and used index.html file

--- js/slide22.js
+++ js/slide22.js
@@ -1,21 +1,3 @@
-//isNumber function
-
-function isNumber (someString){
-    if (parseInt(someString)){
-        return true;
-    } else return false;
-}
-
-console.log(isNumber("abc"));
-
-// converToNumber function
-function converToNumber(someString){
-    return parseInt(someString);
-}
-
-console.log(converToNumber("555"));
-
-
 
 
 
