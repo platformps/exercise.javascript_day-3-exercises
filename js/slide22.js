527c920a47f84543dd42a0a3f639f4a139956d1a
David Frary <fraryd@gmail.com>
2020-07-04 20:50:20 -0400

    finished edits on 'slide22.js'

--- js/slide22.js
+++ js/slide22.js
@@ -1,8 +1,24 @@
 
 
 function isNumber(){
+    let num1 = "";
+    num1 = prompt("enter a number: ");
+    num1 = isFinite(num1);
+    return num1;
 }
+console.log(isNumber())
+
+console.log("function isNumber complete")
 
 
 function convertToNumber(){
+    let num1 = "";
+    num1 = prompt("enter a number: ");
+    num1 = parseFloat(num1);
+    num1 = parseInt(num1);
+    num1 = Number(num1);
+    return num1;
 }
+console.log(convertToNumber())
+
+console.log("function convertToNumber is complete")
